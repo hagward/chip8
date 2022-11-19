adc31b1c96261b13ab8330aa6b80b971b1604ba4
Anders Hagward <taperlawyer@gmail.com>
2022-11-19 11:18:59 +0100

    Add CLI args

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate sdl2;
 
 mod emulator;
 
+use clap::Parser;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -9,6 +10,19 @@ use sdl2::rect::Rect;
 use std::collections::HashMap;
 use std::thread;
 use std::time::{Duration, SystemTime};
+
+/// CHIP-8 emulator
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File name of the ROM file to run
+    #[arg()]
+    file_name: String,
+
+    /// Emulation speed in cycles/frame
+    #[arg(short, long, default_value_t = 27)]
+    speed: usize,
+}
 
 static KEY_MAP: [(Keycode, usize); 16] = [
     (Keycode::Num1, 1),
@@ -30,13 +44,10 @@ static KEY_MAP: [(Keycode, usize); 16] = [
 ];
 
 fn main() {
+    let args = Args::parse();
+
     let mut emulator = emulator::Emulator::new();
-    // emulator.init("./roms/IBM Logo.ch8");
-    emulator.init("./roms/test_opcode.ch8");
-    // emulator.init("./roms/random_number_test.ch8");
-    // emulator.init("./roms/morse_demo.ch8");
-    // emulator.init("./roms/br8kout.ch8");
-    // emulator.init("./roms/chipwar.ch8");
+    emulator.init(&args.file_name);
 
     let key_map = HashMap::from(KEY_MAP);
 
@@ -48,7 +59,7 @@ fn main() {
     let height: usize = pixel_size * 32;
 
     let window = video_subsystem
-        .window("chip8", width as u32, height as u32)
+        .window("CHIP-8", width as u32, height as u32)
         .position_centered()
         .build()
         .unwrap();
@@ -57,12 +68,11 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let frame_time = Duration::from_nanos(1_000_000_000 / 60);
-    let cycles_per_frame = 27;
 
     'running: loop {
         let now = SystemTime::now();
 
-        for _ in 0..cycles_per_frame {
+        for _ in 0..args.speed {
             if emulator.gfx_updated {
                 canvas.set_draw_color(Color::RGB(0, 0, 0));
                 canvas.clear();
